d5f3440c6cd2c782bb70e22731fcf7befed1c16b
Jafit emilio barraza barbosa <jafitbarrazabarbosa@gmail.com>
2025-06-16 19:44:21 -0500

    feat: the scrip for the imc create

---
+++ src/js/main.js
@@ -0,0 +1,14 @@
+let peso
+let altura
+
+peso=prompt("Ingresa tu peso (kg)")
+altura=prompt("Ingrese su altura (Cm)")
+
+imc=peso/(altura*altura)
+
+console.log("Tu imc es "+imc)
+
+//crear una tabla de clasificación
+//condicionales
+// ouput mensahje de condicional segun tu rango de MC
+
